8423100d84d2c5f115fc40b978ea010707743ed1
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-21 20:36:01 +0530

    Profile corrected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar";
 import LogoutButton from "./components/Logout";
+import Profile from "./components/Profile";
 
 function App() {
   return (
