2dda243e2ab9c5d64397cd57300569cb156ceafb
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-21 20:23:42 +0530

    Add login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h4>Hello heroku!!!</h4>
+      <h1>{window.location.origin}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-trial-tenant.us.auth0.com"
+    clientId="nSOCXkx3Qn76MgjHJnlCI4QIUdHjIEyA"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
