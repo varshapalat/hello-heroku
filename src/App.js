75a68cd5bc5e9966821b2766720a47229338f7ab
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-21 20:29:16 +0530

    Logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar";
+import LogoutButton from "./components/Logout";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <LogoutButton />
       <h4>Hello heroku!!!</h4>
       <h1>{window.location.origin}</h1>
     </div>
