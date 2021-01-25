7bdf0030e62994d037c6f6ccd554a7063bba0f3a
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-25 15:28:23 +0530

    Adding profile details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,8 +4,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  return (<div> Profile! {user} {isAuthenticated} {isLoading} End!</div>);
-
   if (isLoading) {
     return <div>Loading ...</div>;
   }
@@ -16,8 +14,15 @@ const Profile = () => {
         <div>Profile section</div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
+        <h3>{user.nickname}</h3>
+        <h3>{user.name}</h3>
+        <h3>{user.picture}</h3>
+        <h3>{user.locale}</h3>
+        <h3>{user.updated_at}</h3>
+        <h3>{user.email}</h3>
+        <h3>{user.email_verified}</h3>
+        <h3>{user.sub}</h3>
         <p>{user.email}</p>
-        <h3>{user}</h3>
       </div>
     )
   );
