455729598035e3668e8b9ac78c2717806b6f08ac
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-27 11:05:19 +0530

    Fix logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <button onClick={() => logout({ returnTo: 'http://localhost:3000/logout' })}>
       Log Out
     </button>
   );
