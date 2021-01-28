07881764bbf08fc1ab0926c957f11f3a0b820308
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-28 12:02:17 +0530

    return to  - remove

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: 'https://dev-heroku-app-varsha.herokuapp.com', federated: true})}>
+    <button onClick={() => logout({federated: true})}>
       Log Out
     </button>
   );
