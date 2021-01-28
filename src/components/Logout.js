413ef6c0f884ad252f35c005770ff72b4edec704
Varsha Palat <varsha.p.aux@go-jek.com>
2021-01-28 11:46:32 +0530

    Add federated

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: 'https://dev-heroku-app-varsha.herokuapp.com/logout' })}>
+    <button onClick={() => logout({ returnTo: 'https://dev-heroku-app-varsha.herokuapp.com/logout?federated' })}>
       Log Out
     </button>
   );
